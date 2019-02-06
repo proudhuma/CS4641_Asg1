affd235f3d1fb380e6f147a833ec79ab4953eb77
proudhuma <54nnxn@163.com>
2019-02-05 20:36:13 -0500

    asg1 related python files and figures

--- asg1/adult/adult.py
+++ asg1/adult/adult.py
@@ -33,14 +33,14 @@ def loadData(filename):
 			eData[column] = le.transform(eData[column])
 	return eData
 	
-def prepareData():
+def prepareData(cross):
 	trainset = loadData("adult.data")
 	testset = loadData("adult.test")
 
 	# seperate data for cross validation
 	trainX, validationX, trainY, validationY = train_test_split(trainset[['age','workclass','fnlwgt','education',
 		'education_num','marital_status','occupation','relationship','race','sex',
-		'capital_gain','capital_loss','hours_per_week','native_country']], trainset['result'], test_size=0.2)
+		'capital_gain','capital_loss','hours_per_week','native_country']], trainset['result'], test_size=cross)
 	testX = testset[['age','workclass','fnlwgt','education',
 		'education_num','marital_status','occupation','relationship','race','sex',
 		'capital_gain','capital_loss','hours_per_week','native_country']]
@@ -52,10 +52,13 @@ def knn(trainX, validationX, trainY, validationY, testX, testY):
 	print("----------- K Nearest Neighbor ------------")
 	tScore = []
 	tknn = []
+	trainAcc = []
 	for k in range(1,30,2):
 		# run knn with different hyperpatameter k
 		knn = KNeighborsClassifier(n_neighbors=k, n_jobs=-1)
 		knn.fit(trainX, trainY)
+		train_acc = knn.score(trainX, trainY)
+		trainAcc.append(train_acc)
 		score = knn.score(validationX, validationY)
 		tScore.append(score)
 		tknn.append(knn)
@@ -66,15 +69,34 @@ def knn(trainX, validationX, trainY, validationY, testX, testY):
 	print("Highest train accuracy: %f with k: %d" % (maxScore, 2*index+1))
 	accuracy = model.score(testX, testY)
 	print("Test accuracy: %f" % (accuracy))
+	accuracy = model.score(testX, testY)
+	print("Test accuracy: %f" % (accuracy))
+	x = range(1,30,2)
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	# axes.set_ylim([0.0,1.0])
+	val, = plt.plot(x,tScore,"b-",linewidth=1)
+	tra, = plt.plot(x,trainAcc,"r-",linewidth=1)
+	val.set_label("Validation")
+	tra.set_label("Train")
+	plt.xlabel("K")
+	plt.ylabel("Accuracy") 
+	plt.title("KNN with Different K")
+	plt.legend()
+	plt.savefig("knn_k.jpg")
+	return model
 		
 def decisionTree(trainX, validationX, trainY, validationY, testX, testY):
 	print("------------- Decision Tree ------------------")
 	scores = []
 	dts = []
+	trainAcc = []
 	# use information gain
 	for max_leaf in range(3,30,3):
 		clf = DecisionTreeClassifier(criterion='entropy', max_leaf_nodes=max_leaf)
 		clf.fit(trainX, trainY)
+		train_acc = clf.score(trainX, trainY)
+		trainAcc.append(train_acc)
 		score = clf.score(validationX, validationY)
 		scores.append(score)
 		dts.append(clf)
@@ -85,16 +107,33 @@ def decisionTree(trainX, validationX, trainY, validationY, testX, testY):
 	print("Highest train accuracy: %f with max leaves: %d" % (maxScore, 3*(index+1)))
 	accuracy = clf.score(testX, testY)
 	print("Test accuracy: %f" %(accuracy))
+	x = range(3,30,3)
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	# axes.set_ylim([0.0,1.0])
+	val, = plt.plot(x,scores,"b-",linewidth=1)
+	tra, = plt.plot(x,trainAcc,"r-",linewidth=1)
+	val.set_label("Validation")
+	tra.set_label("Train")
+	plt.xlabel("Max Leaf")
+	plt.ylabel("Accuracy") 
+	plt.title("Descision Tree with Different Max Leaf")
+	plt.legend()
+	plt.savefig("dt_maxleaf.jpg")
+	return clf
+
 
 def boosting(trainX, validationX, trainY, validationY, testX, testY):
 	# use ada boosting classifier
 	print("------------- Ada Boosting ---------------------")
-
+	trainAcc = []
 	scores = []
 	adas = []
 	for n in range(10,100,10):
 		adacls = AdaBoostClassifier(n_estimators=n)
 		adacls.fit(trainX, trainY)
+		train_acc = adacls.score(trainX, trainY)
+		trainAcc.append(train_acc)
 		score = adacls.score(validationX, validationY)
 		scores.append(score)
 		adas.append(adacls)
@@ -104,16 +143,47 @@ def boosting(trainX, validationX, trainY, validationY, testX, testY):
 	ada = adas[index]	
 	accuracy = ada.score(testX, testY)
 	print("Ada Boosting estimator: %d, test accuracy: %f" % (10*(index+1), accuracy))
-
+	x = range(10,100,10)
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	# axes.set_ylim([0.0,1.0])
+	val, = plt.plot(x,scores,"b-",linewidth=1)
+	tra, = plt.plot(x,trainAcc,"r-",linewidth=1)
+	val.set_label("Validation")
+	tra.set_label("Train")
+	plt.xlabel("Number of Estimator")
+	plt.ylabel("Accuracy") 
+	plt.title("Ada Boosting with Different Number of Estimator")
+	plt.legend()
+	plt.savefig("ada_num_estimator.jpg")
+	return ada
 
 def nn(trainX, validationX, trainY, validationY, testX, testY):
 	print("------------- Neural Network ----------------")
 	nnTrainX = trainX.append(validationX)
 	nnTrainY = trainY.append(validationY)
-	nncls = MLPClassifier(hidden_layer_sizes=(20,5),alpha=0.00005,learning_rate_init=0.0001,early_stopping=True,validation_fraction=0.2)
-	nncls.fit(nnTrainX, nnTrainY)
-	accuracy = nncls.score(testX,testY)
-	print("Neural Network accuracy: %f" %(accuracy))
+	scores = []
+	for x in range(5,27,2):
+		nncls = MLPClassifier(hidden_layer_sizes=(30,x),alpha=0.00005,learning_rate_init=0.0001,early_stopping=True,validation_fraction=0.2)
+		nncls.fit(nnTrainX, nnTrainY)
+		accuracy = nncls.score(testX,testY)
+		scores.append(accuracy)
+	maxScore = max(scores)
+	index = scores.index(maxScore)
+	print("Neural Network accuracy: %f with %d layer" %(accuracy, index*2+5))
+	x = range(5,27,2)
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	axes.set_xlim([0,25])
+	val, = plt.plot(x,scores,"b-",linewidth=1)
+	val.set_label("Test")
+	plt.xlabel("Number of Layer")
+	plt.ylabel("Accuracy") 
+	plt.title("Neural Network with Different Number of Layer")
+	plt.legend()
+	plt.savefig("nn_num_layer.jpg")
+	return nncls
+
 
 def svm(trainX, validationX, trainY, validationY, testX, testY):
 	print("------------- Support Vector Machine ----------")
@@ -127,10 +197,13 @@ def svm(trainX, validationX, trainY, validationY, testX, testY):
 	# four kernel functions
 	kernels = ['poly','linear','rbf','sigmoid']
 	scores = []
+	trainAcc = []
 	clfs = []
 	for kernel in kernels:
 		clf = SVC(gamma='auto', kernel=kernel, cache_size=2000)
 		clf.fit(trainX_scaled, trainY)
+		train_acc = clf.score(trainX_scaled, trainY)
+		trainAcc.append(train_acc)
 		score = clf.score(validationX_scaled, validationY)
 		scores.append(score)
 		clfs.append(clf)
@@ -141,12 +214,26 @@ def svm(trainX, validationX, trainY, validationY, testX, testY):
 	accuracy = svm.score(testX_scaled, testY)
 	print("Highest train accuracy: %f with kernel: " % (maxScore) + kernels[index])
 	print("Test accuracy: %f" %(accuracy))
-
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	# axes.set_ylim([0.0,1.0])
+	x=[1,2,3,4]
+	val, = plt.plot(x,scores,"b-",linewidth=1)
+	tra, = plt.plot(x,trainAcc,"r-",linewidth=1)
+	plt.xticks(x, kernels)
+	val.set_label("Validation")
+	tra.set_label("Train")
+	plt.xlabel("Kernel")
+	plt.ylabel("Accuracy") 
+	plt.title("Support Vector Machine with Different Kernels")
+	plt.legend()
+	plt.savefig("svm_kernel.jpg")
+	return svm
 
 if __name__ == "__main__":
-	trainX, validationX, trainY, validationY, testX, testY = prepareData()
+	trainX, validationX, trainY, validationY, testX, testY = prepareData(0.2)
 	knn(trainX, validationX, trainY, validationY, testX, testY)
-	svm(trainX, validationX, trainY, validationY, testX, testY)
 	decisionTree(trainX, validationX, trainY, validationY, testX, testY)
 	boosting(trainX, validationX, trainY, validationY, testX, testY)
 	nn(trainX, validationX, trainY, validationY, testX, testY)
+	svm(trainX, validationX, trainY, validationY, testX, testY)
--- asg1/wine/wine.py
+++ asg1/wine/wine.py
@@ -17,9 +17,9 @@ if not sys.warnoptions:
     import warnings
     warnings.simplefilter("ignore")
 	
-def prepareData():
+def prepareData(cross):
 	wine = datasets.load_wine()
-	trainX, testX, trainY, testY = train_test_split(wine.data, wine.target, test_size=0.2)
+	trainX, testX, trainY, testY = train_test_split(wine.data, wine.target, test_size=cross)
 
 	return trainX, trainY, testX, testY
 
@@ -27,12 +27,15 @@ def knn(trainX, trainY, testX, testY):
 	print("----------- K Nearest Neighbor ------------")
 	tScore = []
 	tknn = []
+	trainAcc = []
 	for k in range(1,30,2):
 		# run knn with different hyperpatameter k
 		knn = KNeighborsClassifier(n_neighbors=k, n_jobs=-1)
 		knn.fit(trainX, trainY)
+		train_acc = knn.score(trainX, trainY)
 		score = knn.score(testX, testY)
 		tScore.append(score)
+		trainAcc.append(train_acc)
 		tknn.append(knn)
 		print("k: %d, accuracy: %f" %(k, score))
 	maxScore = max(tScore)
@@ -41,15 +44,33 @@ def knn(trainX, trainY, testX, testY):
 	print("Highest train accuracy: %f with k: %d" % (maxScore, 2*index+1))
 	accuracy = model.score(testX, testY)
 	print("Test accuracy: %f" % (accuracy))
+	x = range(1,30,2)
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	# axes.set_ylim([0.0,1.0])
+	val, = plt.plot(x,tScore,"b-",linewidth=1)
+	tra, = plt.plot(x,trainAcc,"r-",linewidth=1)
+	val.set_label("Validation")
+	tra.set_label("Train")
+	plt.xlabel("K")
+	plt.ylabel("Accuracy") 
+	plt.title("KNN with Different K")
+	plt.legend()
+	plt.savefig("knn_k.jpg")
+	return model
+
 		
 def decisionTree(trainX, trainY, testX, testY):
 	print("------------- Decision Tree ------------------")
 	scores = []
 	dts = []
+	trainAcc = []
 	# use information gain
 	for max_leaf in range(3,30,3):
 		clf = DecisionTreeClassifier(criterion='entropy', max_leaf_nodes=max_leaf)
 		clf.fit(trainX, trainY)
+		train_acc = clf.score(trainX, trainY)
+		trainAcc.append(train_acc)
 		score = clf.score(testX, testY)
 		scores.append(score)
 		dts.append(clf)
@@ -60,16 +81,32 @@ def decisionTree(trainX, trainY, testX, testY):
 	print("Highest train accuracy: %f with max leaves: %d" % (maxScore, 3*(index+1)))
 	accuracy = clf.score(testX, testY)
 	print("Test accuracy: %f" %(accuracy))
+	x = range(3,30,3)
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	# axes.set_ylim([0.0,1.0])
+	val, = plt.plot(x,scores,"b-",linewidth=1)
+	tra, = plt.plot(x,trainAcc,"r-",linewidth=1)
+	val.set_label("Validation")
+	tra.set_label("Train")
+	plt.xlabel("Max Leaf")
+	plt.ylabel("Accuracy") 
+	plt.title("Descision Tree with Different Max Leaf")
+	plt.legend()
+	plt.savefig("dt_maxleaf.jpg")
+	return clf
 
 def boosting(trainX, trainY, testX, testY):
 	# use ada boosting classifier
 	print("------------- Ada Boosting ---------------------")
-
 	scores = []
+	trainAcc = []
 	adas = []
 	for n in range(10,100,10):
 		adacls = AdaBoostClassifier(n_estimators=n)
 		adacls.fit(trainX, trainY)
+		train_acc = adacls.score(trainX, trainY)
+		trainAcc.append(train_acc)
 		score = adacls.score(testX, testY)
 		scores.append(score)
 		adas.append(adacls)
@@ -79,16 +116,47 @@ def boosting(trainX, trainY, testX, testY):
 	ada = adas[index]	
 	accuracy = ada.score(testX, testY)
 	print("Ada Boosting estimator: %d, test accuracy: %f" % (10*(index+1), accuracy))
+	x = range(10,100,10)
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	# axes.set_ylim([0.0,1.0])
+	val, = plt.plot(x,scores,"b-",linewidth=1)
+	tra, = plt.plot(x,trainAcc,"r-",linewidth=1)
+	val.set_label("Validation")
+	tra.set_label("Train")
+	plt.xlabel("Number of Estimator")
+	plt.ylabel("Accuracy") 
+	plt.title("Ada Boosting with Different Number of Estimator")
+	plt.legend()
+	plt.savefig("ada_num_estimator.jpg")
+	return ada
 
 
 def nn(trainX, trainY, testX, testY):
 	print("------------- Neural Network ----------------")
 	nnTrainX = trainX
 	nnTrainY = trainY
-	nncls = MLPClassifier(hidden_layer_sizes=(30,7),alpha=0.00005,learning_rate_init=0.0001,early_stopping=True,validation_fraction=0.2)
-	nncls.fit(nnTrainX, nnTrainY)
-	accuracy = nncls.score(testX,testY)
-	print("Neural Network accuracy: %f" %(accuracy))
+	scores = []
+	for x in range(5,27,2):
+		nncls = MLPClassifier(hidden_layer_sizes=(30,x),alpha=0.00005,learning_rate_init=0.0001,early_stopping=True,validation_fraction=0.2)
+		nncls.fit(nnTrainX, nnTrainY)
+		accuracy = nncls.score(testX,testY)
+		scores.append(accuracy)
+	maxScore = max(scores)
+	index = scores.index(maxScore)
+	print("Neural Network accuracy: %f with %d layer" %(accuracy, index*2+5))
+	x = range(5,27,2)
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	axes.set_xlim([0,25])
+	val, = plt.plot(x,scores,"b-",linewidth=1)
+	val.set_label("Test")
+	plt.xlabel("Number of Layer")
+	plt.ylabel("Accuracy") 
+	plt.title("Neural Network with Different Number of Layer")
+	plt.legend()
+	plt.savefig("nn_num_layer.jpg")
+	return nncls
 
 def svm(trainX, trainY, testX, testY):
 	print("------------- Support Vector Machine ----------")
@@ -101,10 +169,13 @@ def svm(trainX, trainY, testX, testY):
 	# four kernel functions
 	kernels = ['poly','linear','rbf','sigmoid']
 	scores = []
+	trainAcc = []
 	clfs = []
 	for kernel in kernels:
 		clf = SVC(gamma='auto', kernel=kernel, cache_size=2000)
 		clf.fit(trainX_scaled, trainY)
+		train_acc = clf.score(trainX_scaled, trainY)
+		trainAcc.append(train_acc)
 		score = clf.score(testX_scaled, testY)
 		scores.append(score)
 		clfs.append(clf)
@@ -115,12 +186,27 @@ def svm(trainX, trainY, testX, testY):
 	accuracy = svm.score(testX_scaled, testY)
 	print("Highest train accuracy: %f with kernel: " % (maxScore) + kernels[index])
 	print("Test accuracy: %f" %(accuracy))
-
+	plt.figure(figsize=(8,4))
+	axes = plt.gca()
+	# axes.set_ylim([0.0,1.0])
+	x=[1,2,3,4]
+	val, = plt.plot(x,scores,"b-",linewidth=1)
+	tra, = plt.plot(x,trainAcc,"r-",linewidth=1)
+	plt.xticks(x, kernels)
+	val.set_label("Validation")
+	tra.set_label("Train")
+	plt.xlabel("Kernel")
+	plt.ylabel("Accuracy") 
+	plt.title("Support Vector Machine with Different Kernels")
+	plt.legend()
+	plt.savefig("svm_kernel.jpg")
+	return svm
 
 if __name__ == "__main__":
-	trainX, trainY, testX, testY = prepareData()
-	knn(trainX, trainY, testX, testY)
-	svm(trainX, trainY, testX, testY)
-	decisionTree(trainX, trainY, testX, testY)
-	boosting(trainX, trainY, testX, testY)
-	nn(trainX, trainY, testX, testY)
+	# use 20% train data as validation data
+	trainX, trainY, testX, testY = prepareData(0.2)
+	knn = knn(trainX, trainY, testX, testY)
+	svm = svm(trainX, trainY, testX, testY)
+	dt = decisionTree(trainX, trainY, testX, testY)
+	boosting = boosting(trainX, trainY, testX, testY)
+	nn = nn(trainX, trainY, testX, testY)
